1039e21af82ea47232c40ff097fc0175a8e913c8
erfengwelink <erfengwelink@welink.io>
2018-06-26 21:40:54 +0800

    add block demo

---
+++ 04-task-blocked/taskBlocked/main/task-blocked.c
@@ -0,0 +1,82 @@
+/* Hello World Example
+
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "esp_spi_flash.h"
+
+#define mainDELAY_LOOP_COUNT		( 0xfffffff )
+
+const char *pcTextForTask1 = "Task 1 is running\n";
+const char *pcTextForTask2 = "Task 2 is running\n";
+
+
+void vTaskFunction( void *pvParameters )
+{
+char *pcTaskName;
+//volatile unsigned long ul;
+
+	/* The string to print out is passed in via the parameter.  Cast this to a
+	character pointer. */
+	pcTaskName = ( char * ) pvParameters;
+
+	/* As per most tasks, this task is implemented in an infinite loop. */
+	for( ;; )
+	{
+		/* Print out the name of this task. */
+		printf("%s\n", pcTaskName );
+
+#if 0
+		/* Delay for a period. */
+		for( ul = 0; ul < mainDELAY_LOOP_COUNT; ul++ )
+		{
+			/* This loop is just a very crude delay implementation.  There is
+			nothing to do in here.  Later exercises will replace this crude
+			loop with a proper delay/sleep function. */
+		}
+#endif
+		vTaskDelay(250 / portTICK_RATE_MS);
+	}
+}
+
+void app_main()
+{
+    printf("Hello world!\n");
+
+    /* Print chip information */
+    esp_chip_info_t chip_info;
+    esp_chip_info(&chip_info);
+    printf("This is ESP32 chip with %d CPU cores, WiFi%s%s, ",
+            chip_info.cores,
+            (chip_info.features & CHIP_FEATURE_BT) ? "/BT" : "",
+            (chip_info.features & CHIP_FEATURE_BLE) ? "/BLE" : "");
+
+    printf("silicon revision %d, ", chip_info.revision);
+
+    printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
+            (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded\n" : "external\n");
+
+    /* Create one of the two tasks. */
+	xTaskCreate(	vTaskFunction,		/* Pointer to the function that implements the task. */
+					"vTaskFunction",	/* Text name for the task.  This is to facilitate debugging only. */
+					1024,		/* Stack depth in words. */
+					(void*)pcTextForTask1,		/* We are not using the task parameter. */
+					1,			/* This task will run at priority 1. */
+					NULL );		/* We are not using the task handle. */
+
+
+    xTaskCreate(	vTaskFunction,		/* Pointer to the function that implements the task. */
+					"vTaskFunction",	/* Text name for the task.  This is to facilitate debugging only. */
+					1024,		/* Stack depth in words. */
+					(void*)pcTextForTask2,		/* We are not using the task parameter. */
+					2,			/* This task will run at priority 1. */
+					NULL );		/* We are not using the task handle. */
+
+}
