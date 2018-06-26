53ede4ac64ad6814fc9b55f2ef53de04c0420e2d
erfengwelink <erfengwelink@welink.io>
2018-06-26 20:53:10 +0800

    add taskCreate demo

---
+++ 01-task-create/taskCreate/main/task-create.c
@@ -0,0 +1,97 @@
+/* Hello World Example
+
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "esp_spi_flash.h"
+
+#define mainDELAY_LOOP_COUNT		( 0xfffffff )
+
+void vTask1( void *pvParameters )
+{
+	char *pcTaskName = "Task 1 is running\r\n";
+	volatile unsigned long ul;
+
+	/* As per most tasks, this task is implemented in an infinite loop. */
+	for( ;; )
+	{
+		/* Print out the name of this task. */
+		printf("%s\n", pcTaskName );
+
+		/* Delay for a period. */
+#if 0
+		for( ul = 0; ul < mainDELAY_LOOP_COUNT; ul++ )
+		{
+			/* This loop is just a very crude delay implementation.  There is
+			nothing to do in here.  Later exercises will replace this crude
+			loop with a proper delay/sleep function. */
+		}
+#endif
+
+		vTaskDelay(500 / portTICK_RATE_MS);
+	}
+}
+
+void vTask2( void *pvParameters )
+{
+	char *pcTaskName = "Task 2 is running\r\n";
+	volatile unsigned long ul;
+
+	/* As per most tasks, this task is implemented in an infinite loop. */
+	for( ;; )
+	{
+		/* Print out the name of this task. */
+		printf("%s\n", pcTaskName );
+
+		/* Delay for a period. */
+#if 0
+		for( ul = 0; ul < mainDELAY_LOOP_COUNT; ul++ )
+		{
+			/* This loop is just a very crude delay implementation.  There is
+			nothing to do in here.  Later exercises will replace this crude
+			loop with a proper delay/sleep function. */
+		}
+#endif
+	vTaskDelay(250 / portTICK_RATE_MS);
+	}
+}
+
+void app_main()
+{
+    printf("Hello world!\n");
+
+    /* Print chip information */
+    esp_chip_info_t chip_info;
+    esp_chip_info(&chip_info);
+    printf("This is ESP32 chip with %d CPU cores, WiFi%s%s, ",
+            chip_info.cores,
+            (chip_info.features & CHIP_FEATURE_BT) ? "/BT" : "",
+            (chip_info.features & CHIP_FEATURE_BLE) ? "/BLE" : "");
+
+    printf("silicon revision %d, ", chip_info.revision);
+
+    printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
+            (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
+
+    /* Create one of the two tasks. */
+	xTaskCreate(	vTask1,		/* Pointer to the function that implements the task. */
+					"Task 1",	/* Text name for the task.  This is to facilitate debugging only. */
+					1024,		/* Stack depth in words. */
+					NULL,		/* We are not using the task parameter. */
+					1,			/* This task will run at priority 1. */
+					NULL );		/* We are not using the task handle. */
+
+	/* Create the other task in exactly the same way. */
+	xTaskCreate( vTask2, "Task 2", 1024, NULL, 1, NULL );
+
+    /* Start the scheduler so our tasks start executing. */
+	//vTaskStartScheduler();
+    
+}
